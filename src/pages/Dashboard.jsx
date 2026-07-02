7cc18d85bd6f05c7a35eb5ef96f500e02b170656
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-07-02 15:10:37 +0545

    Dashboard Setup

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,21 @@
-function Dashboard() {
-    return <h1>Dashboard</h1>;
+import {useState} from "react";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+
+function Dashboard(){
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    return(
+        <div className="dashboard-layout">
+            <Navbar toggleSidebar={()=>setShowSidebar(!showSidebar)}/>
+            
+            {showSidebar && <Sidebar/>}
+
+            <main className="dashboard-content">
+                <h1>Dashboard</h1>
+            </main>
+        </div>
+    );
 }
 
 export default Dashboard;
