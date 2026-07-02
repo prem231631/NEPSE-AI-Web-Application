6d85ec6321ec3a4c02403e8b3b3d443fab8f3110
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-07-02 15:33:26 +0545

    dashboardNavbar CSS added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,21 +3,20 @@ import "./../styles/sidebar.css";
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-
-      <h2>NEPSE AI</h2>
-
+    <aside className="sidebar">
       <Link to="/dashboard">Dashboard</Link>
 
-      <Link to="/market">Market</Link>
+      <Link to ="/market">Market</Link>
 
-      <Link to="/prediction">AI Prediction</Link>
+      <Link to ="/prediction">AI Prediction</Link>
 
-      <Link to="/watchlist">Watchlist</Link>
+      <Link to ="/watchlist">Watchlist</Link>
 
-      <Link to="/settings">Settings</Link>
+      <Link to ="/notification">Notification</Link>
 
-    </div>
+      <link to ="/settings">Settings</link>
+      
+    </aside>
   );
 }
 
