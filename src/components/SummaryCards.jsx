bba2b2d4c6026f28e4d6013f30144fe73d3a5c6f
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-07-09 18:15:40 +0545

    Market Status Improved with green and red icon

--- src/components/SummaryCards.jsx
+++ src/components/SummaryCards.jsx
@@ -8,6 +8,27 @@ import {
 import "../styles/summaryCards.css";
 
 function SummaryCards() {
+    const now = new Date();
+
+    const nepalTime = new Date(
+        now.toLocaleString("en-US", {
+            timeZone: "Asia/Kathmandu",
+        })
+    );
+
+    const day = nepalTime.getDay();      // 0 = Sunday, 6 = Saturday
+    const hour = nepalTime.getHours();
+    const minute = nepalTime.getMinutes();
+
+    const currentTime = hour * 60 + minute;
+
+    const marketOpen =
+        day >= 1 &&
+        day <= 5 &&
+        currentTime >= (11 * 60) &&
+        currentTime < (15 * 60);
+
+    const marketStatus = marketOpen ? "Open" : "Closed";
 
     const cards = [
         {
@@ -25,8 +46,8 @@ function SummaryCards() {
         {
             icon: <FiActivity />,
             title: "Market Status",
-            value: "Open",
-            change: "Live"
+            value: marketStatus,
+            change: marketOpen ? "Live" : "Closed"
         },
         {
             icon: <FiStar />,
@@ -40,15 +61,29 @@ function SummaryCards() {
         <section className="summary-cards">
             {cards.map((card, index) => (
                 <div className="summary-card" key={index}>
-                    <div className="card-icon">
-                        {card.icon}
+                    <div
+                        className={`card-icon ${
+                            card.title === "Market Status"
+                            ? (marketOpen ? "icon-open" : "icon-closed")
+                            : ""
+                        }`}
+                    >
+                    {card.icon}
                     </div>
 
                     <h4>{card.title}</h4>
 
                     <h2>{card.value}</h2>
 
-                    <p>{card.change}</p>
+                    <p
+                        className={
+                            card.title === "Market Status"
+                            ? (marketOpen ? "market-open" : "market-closed")
+                            : ""
+                        }
+                    >       
+                        {card.change}
+                    </p>
                 </div>
             ))}
         </section>
