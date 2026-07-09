9932f9db904dd240c4fed493447aa0da49bc2bb9
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-07-09 16:29:27 +0545

    WelcomeBanner Setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
 function Login() {
     return(
-        <div id="login"className="login-page">
+        <div className="login-page">
             <div className="login-card">
                 {/* logo */}
                 <div className="login-logo">
