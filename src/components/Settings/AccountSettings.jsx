80347eca386dfa363c4d3e08f73ad54887a8e6b6
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-07-15 16:17:51 +0545

    AccountSetting Component Added

---
+++ src/components/Settings/AccountSettings.jsx
@@ -0,0 +1,35 @@
+import { FiUser } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+function AccountSettings({ user }) {
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="settings-card account-card">
+            <div className="account-left">
+                <div className="account-avatar">
+                    {user.name.charAt(0).toUpperCase()}
+                </div>
+
+                <div>
+                    <h2>{user.name}</h2>
+
+                    <p>{user.email}</p>
+
+                    <span>Age : {user.age}</span>
+                </div>
+            </div>
+
+            <button
+                className="settings-btn"
+                onClick={() => navigate("/profile")}
+            >
+                <FiUser />
+                Edit Profile
+            </button>
+        </div>
+    );
+}
+
+export default AccountSettings;
