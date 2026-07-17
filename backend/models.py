27a71fece6abe691247a6cf0d56a57d342b76824
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-07-17 12:37:07 +0545

    OTP Code Mechanism Setup and Watchlist update

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
+from datetime import datetime
 
 
 class User(Base): 
@@ -10,3 +11,12 @@ class User(Base):
     age = Column(Integer, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
+
+class OTPStore(Base):
+    __tablename__ = "otp_store"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), nullable=False)
+    otp = Column(String(6), nullable=False)
+    expires_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
--- backend/watchlist.py
+++ backend/watchlist.py
@@ -8,7 +8,7 @@ fake_watchlist= {}
 
 @router.get("/watchlist")
 def get_watchlist(current_user=Depends(get_current_user)):
-    email = current_user["email"]
+    email = current_user.email
     symbols = fake_watchlist.get(email, [])
     
     if not symbols:
@@ -28,11 +28,47 @@ def get_watchlist(current_user=Depends(get_current_user)):
     }
 
 
-#@router.post("/watchlist/{symbol}")
+@router.post("/watchlist/{symbol}")
+def add_to_watchlist(symbol: str, current_user=Depends(get_current_user)):
+    email = current_user.email
+    symbol = symbol.upper()
+
+    if symbol not in fake_stock_data:
+        raise HTTPException(status_code=404, detail=f"Stock '{symbol}' not found")
+
+    if email not in fake_watchlist:
+        fake_watchlist[email] = []          
+
+    if symbol in fake_watchlist[email]:     
+        raise HTTPException(status_code=409, detail=f"'{symbol}' already in watchlist")
+
+    fake_watchlist[email].append(symbol)    
+
+    return {
+        "message": f"{symbol} added to watchlist",
+        "watchlist": fake_watchlist[email]
+    }
 
 
-#@router.delete("/watchlist/{symbol}")
+@router.delete("/watchlist/{symbol}")
+def remove_from_watchlist(symbol: str, current_user=Depends(get_current_user)):
+    email = current_user.email
+    symbol = symbol.upper()
 
-#@router.delete("/watchlist")
+    if email not in fake_watchlist or symbol not in fake_watchlist[email]:
+        raise HTTPException(status_code=404, detail=f"'{symbol}' not in your watchlist")
+
+    fake_watchlist[email].remove(symbol)
+
+    return {
+        "message": f"{symbol} removed from watchlist",
+        "watchlist": fake_watchlist[email]
+    }
 
 
+#To clear entire watchlist
+@router.delete("/watchlist")
+def clear_watchlist(current_user=Depends(get_current_user)):
+    email = current_user.email
+    fake_watchlist[email] = []
+    return {"message": "Watchlist cleared"}
